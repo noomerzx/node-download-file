4c3a0ab8657d616f8ed2fa21d14c13fb13df21af
Wisarut <wisarut.p@linecorp.com>
2020-02-24 17:03:51 +0700

    update http and ftp provider

--- providers/ftp-server.js
+++ providers/ftp-server.js
@@ -1,1 +1,25 @@
-// to be implementation
+const FtpSvr = require ( 'ftp-srv' )
+
+const hostname = '127.0.0.1'
+const port = 22
+const options = {
+  url: 'ftp://' + hostname + ':' + port,
+  pasv_url: hostname,
+  anonymous: true,
+  greeting: 'Hi Welcome to FTP Server !!'
+}
+
+const ftpServer = new FtpSvr (options)
+
+ftpServer.on ('login', ( data, resolve, reject ) => {
+  resolve({root: './providers/files'})
+})
+
+ftpServer.on ('client-error', (connection, context, error) => {
+  console.log('error')
+})
+
+
+ftpServer.listen().then(() => {
+  console.log (`Server running at http://${hostname}:${port}/`)
+})
--- providers/http-server.js
+++ providers/http-server.js
@@ -1,1 +1,16 @@
-// to be implementation
+const fs = require('fs')
+const server = require('http').createServer()
+
+server.on('request', (req, res) => {
+  const stream = fs.createReadStream('./providers/files/doublebig.file')
+  stream.pipe(res)
+})
+
+// server.on('request', (req, res) => {
+//   fs.readFile('./providers/files/doublebig.file', (err, data) => {
+//     if (err) throw err;
+//     res.end(data)
+//   })
+// })
+
+server.listen(8001)
